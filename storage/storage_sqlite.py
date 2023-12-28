25e2e4833e43eb0ba31a5afda9404931b1847cdd
Nikita-Kolbin <kolbin.niclita2014@yandex.ru>
2023-12-28 17:02:27 +0500

    edit storage and requirements

--- storage/storage_sqlite.py
+++ storage/storage_sqlite.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from pathlib import Path
 
 
@@ -6,6 +7,8 @@ class Storage:
     def __init__(self):
         self.con = sqlite3.connect(rf'{Path.cwd().absolute()}\storage\statistics.db')
         self.cur = self.con.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS statistics (ID integer primary key AUTOINCREMENT, "
+                         "Date VARCHAR(20), Language VARCHAR(20), Speed VARCHAR(20), Accuracy VARCHAR(20))")
 
     def add_train(self, date, lang, speed, accuracy):
         self.cur.execute(f"INSERT INTO statistics (Date, Language, Speed, Accuracy) "
